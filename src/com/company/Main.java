6c5d9d75291db6ea32fca8232ca56dcf5852987f
vmalarcon <vmalarcon@gmail.com>
2019-10-24 19:19:53 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.*;
+import javax.sound.sampled.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            File yourFile;
+            AudioInputStream stream;
+            AudioFormat format;
+            DataLine.Info info;
+            Clip clip;
+
+            stream = AudioSystem.getAudioInputStream(new File("C:\\Windows\\Media\\Ring05.wav"));
+            format = stream.getFormat();
+            info = new DataLine.Info(Clip.class, format);
+            clip = (Clip) AudioSystem.getLine(info);
+            clip.open(stream);
+            clip.start();
+            Thread.sleep(20000L);
+        }
+        catch (Exception e) {
+            //whatevers
+        }
+    }
+}
